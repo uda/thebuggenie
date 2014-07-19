057eae18244f79238b8bf9a391f7b60915c33394
Yehuda Deutsch <yeh@uda.co.il>
2014-07-19 12:50:00 +0300

    duplicate issues issue

--- core/classes/TBGIssue.class.php
+++ core/classes/TBGIssue.class.php
@@ -2093,7 +2093,7 @@
 			if ($this->_duplicate_issues === null)
 			{
 				$this->_b2dbLazyload('_duplicate_issues');
-				foreach ($this->_duplicate_issues as $issue_id => $issue)
+				if ($this->_duplicate_issues) foreach ($this->_duplicate_issues as $issue_id => $issue)
 				{
 					if (!$issue->hasAccess()) unset($this->_duplicate_issues[$issue_id]);
 				}
